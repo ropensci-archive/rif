de63ec38c9fffed9a8143390ea9d7bf833959653
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-08 09:02:52 -0800

    added all initial files

---
+++ R/rif-package.R
@@ -0,0 +1,8 @@
+#' R Client for Neuroscience Information Framework APIs
+#'
+#' @name rif-package
+#' @aliases rif
+#' @docType package
+#' @title R Client for Neuroscience Information Framework APIs
+#' @keywords package
+NULL
