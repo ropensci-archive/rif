84a77c1d3b35c37cdfb3b17794da400fcd321a69
steven2249 <stevenysw@berkeley.edu>
2017-03-13 22:00:41 -0700

    Update test-federation_data.R

--- tests/testthat/test-federation_data.R
+++ tests/testthat/test-federation_data.R
@@ -1,5 +1,15 @@
 context("federation_data")
 
 test_that("federation_data", {
-  aa <- federation_data(id = )
+  tt <- federation_data(id = "nlx_152871-2")
+
+  expect_is(tt, "list")
+  expect_is(tt$result, "list")
+  expect_is(tt$result$resultCount, "integer")
+  expect_match(tt$result$nifId, "nlx_152871-2")
+  expect_is(tt$result$result, "data.frame")
+  expect_is(tt$result$debugInfo, "character")
+  expect_is(tt$result$results, "data.frame")
+  expect_is(tt$query, "data.frame")
+  expect_is(tt$messages, "list")
 })
